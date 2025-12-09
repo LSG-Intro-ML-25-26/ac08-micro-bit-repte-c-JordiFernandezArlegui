7f16e4f7c26d9022d9c8421af1a004f1ed3363b5
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-12-09 19:45:40 +0100

    random num

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+num = 0
+String2 = ""
+
+def on_received_number(receivedNumber):
+    if num > 3:
+        basic.show_icon(IconNames.HAPPY)
+    else:
+        basic.show_icon(IconNames.SAD)
+radio.on_received_number(on_received_number)
+
 def on_button_pressed_a():
-    radio.send_string("" + str((String2)))
+    global String2
+    String2 = "String"
+    radio.send_string(String2)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_string(receivedString):
-    pass
+    basic.show_string(String2)
 radio.on_received_string(on_received_string)
 
-String2 = 0
-String2 = 0
+def on_button_pressed_b():
+    global num
+    num += randint(1, 6)
+    radio.send_number(num)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (num > 3) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString(String2)
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(String2)
 })
+input.onButtonPressed(Button.B, function () {
+    num += randint(1, 6)
+    radio.sendNumber(num)
+})
+let num = 0
 let String2 = ""
 String2 = "String"
